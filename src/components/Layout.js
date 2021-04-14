43868e7c693a16c88485024d092103deb9bfe503
Felipe Merchan <felipe-sc@hotmail.es>
2021-04-13 22:21:23 -0500

    Se creó el componente List

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Logo from './Logo'
+import List from './List'
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Logo />
+      <List />
       <div>
         {children}
       </div>
---
+++ src/components/List.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import Link from 'next/link'
+
+const List = () => {
+  return (
+    <>
+      <nav className="list" aria-labelledby="library-navigation">
+        <h2 className="list-title" id="library-navigation">Tu Biblioteca</h2>
+        <ul className="list-content">
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Creada para ti
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Escuchado recientemente
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Tus Me Gusta
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="list" aria-labelledby="playlist-navigation">
+        <h2 className="list-title" id="playlist-navigation">Playlists</h2>
+        <ul className="list-content">
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Lo mejor de los mejor
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Naruto al 100% para progr…
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Sin parar
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Naruto openings
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Boku no pico
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                This is Pokémon
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Más populares de la semana
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Mis pistas de shazam
+              </a>
+            </Link>
+          </li>
+          <li className="list-item">
+            <Link href="/">
+              <a className="link" aria-current="page">
+                Audio Library
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default List
