a3b0286ff4f0492a3ebd7efc872ae282bbeb9d58
Felipe Merchan <felipe-sc@hotmail.es>
2021-04-24 23:16:32 -0500

    Se agregó el Open Graph

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,13 @@ class MyDocument extends Document {
       <Html lang="es">
         <Head>
           <meta name="robots" content="index,follow" />
-          <meta name="description" content="" />
+          <meta name="description" content="Interfaz de Spotifu, una de las apps más famosas de la industria musical." />
           <meta name="keywords" content="Música, playlist, streaming" />
+          <meta property="og:title" content="Mi proyecto del curso de flexbox layout y componentes" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Interfaz de Spotifu, una de las apps más famosas de la industria musical." />
+          <meta property="og:url" content="https://redesign-spotifu.vercel.app/" />
+          <meta property="og:image" content="https://res.cloudinary.com/dy1xcx7kw/image/upload/v1619323822/spotifu/open-graph-spotifu_nqztzl.jpg" />
         </Head>
         <body className="body">
           <Main />
