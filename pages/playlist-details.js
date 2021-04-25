979905d7a32cd58c55eb45caaccbe2ad233321d3
Felipe Merchan <felipe-sc@hotmail.es>
2021-04-24 22:50:54 -0500

    Se eliminó el import de los estilos de los componentes página

--- pages/playlist-details.js
+++ pages/playlist-details.js
@@ -4,8 +4,6 @@ import React from 'react'
 import DataTable from '../src/components/DataTable' */
 import data from '../data.json'
 
-import styles from '../src/assets/styles/pages/playlist-details.module.scss'
-
 const Playlist = () => {
   return (
     <main className={styles.Playlist}>
