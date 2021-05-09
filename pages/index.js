feeed9622322d93d382e6a09c5052d17851b5250
Felipe Merchan <felipe-sc@hotmail.es>
2021-05-09 15:32:19 -0500

    Se creó el componente PlaylistB

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import data from '../data.json'
 import PlaylistA from '../src/components/PlaylistA'
+import PlaylistB from '../src/components/PlaylistB'
 
 const Home = () => {
   return (
     <>
       <main>
         <PlaylistA />
+        <PlaylistB />
       </main>
     </>
   )
