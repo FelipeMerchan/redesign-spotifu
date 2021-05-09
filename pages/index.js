b803154e7d54ac10e1535b1eb403575b6f116722
Felipe Merchan <felipe-sc@hotmail.es>
2021-05-09 16:46:02 -0500

    Se creó el componente PlaylistList con el fin de que sea el contenedor de PlaylistA y PlaylistB

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@ import React from 'react'
 import data from '../data.json'
 import PlaylistA from '../src/components/PlaylistA'
 import PlaylistB from '../src/components/PlaylistB'
+import PlaylistList from '../src/components/PlaylistList'
 
 const Home = () => {
   return (
     <>
       <main>
-        <PlaylistA />
-        <PlaylistB />
+        <PlaylistList region="region1" title="Buenas noches">
+          <PlaylistB />
+          <PlaylistB />
+          <PlaylistB />
+          <PlaylistB />
+          <PlaylistB />
+        </PlaylistList>
+        <PlaylistList region="region2" title="Escuchado recientemente">
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+          <PlaylistA />
+        </PlaylistList>
       </main>
     </>
   )
